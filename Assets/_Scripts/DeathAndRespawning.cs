1aba1b26d9d397321800259ad26141e889da009c
Franz <franzjacobh@gmail.com>
2018-01-27 15:56:11 -0800

    previous changes contained wrong files. Same changes, right files this time

---
+++ Assets/_Scripts/DeathAndRespawning.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathAndRespawning : MonoBehaviour {
+
+    private bool death = false;
+    private Vector3 spawnPosition;
+    private IEnumerator coroutine;
+    private GameObject deathLocation;
+
+	void Start () {
+        spawnPosition = transform.position;
+        deathLocation = GameObject.FindWithTag("DeathLocation");
+	}
+	
+	void Update () {
+
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Monster")
+        {
+            Debug.Log("Collision occured: initiate death sequence");
+            death = true;
+            Debug.Log(death);
+            coroutine = Respawn();
+            StartCoroutine(coroutine);
+            //_respawn();
+        }
+    }
+
+    /*
+    private void _respawn()
+    {
+        if (death)
+        {
+            transform.position = spawnPosition;
+            death = false;
+        }
+    }
+    */
+
+    private IEnumerator Respawn()
+    {
+        if (death)
+        {
+            yield return new WaitForSeconds(3);
+            transform.position = deathLocation.transform.position;
+            death = false;
+        }
+    }
+}
---
+++ Assets/_Scripts/MonsterPathfinding.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterPathfinding : MonoBehaviour {
+
+    public float moveSpeed = 3f;
+    public float rotationSpeed = 3f;
+
+    private Transform m_Transform;
+    private Transform target;
+    private NavMeshAgent agent;
+    private Vector3 destination;
+
+    private void Awake()
+    {
+        m_Transform = gameObject.transform;
+    }
+
+    void Start ()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+        if (!target)
+        {
+            Debug.Log("YOU FORGOT TO ADD THE PLAYER OBJECT");
+        }
+
+        agent = GetComponent<NavMeshAgent>();
+        destination = agent.destination;
+	}
+	
+	void Update ()
+    {
+        destination = target.position;
+        agent.destination = destination;
+        /*
+        m_Transform.rotation = Quaternion.Slerp(m_Transform.rotation,
+            Quaternion.LookRotation(target.position - m_Transform.position),
+            rotationSpeed * Time.deltaTime);
+
+        m_Transform.position += m_Transform.forward * moveSpeed * Time.deltaTime;
+	    */
+    }
+}
