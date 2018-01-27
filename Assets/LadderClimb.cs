c1ca4ed0fd6d3d507a94763cda9852d0eee329fa
Max Klassen <maximiliankk@gmail.com>
2018-01-27 08:30:22 -0800

    Initial files

---
+++ Assets/LadderClimb.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderClimb : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            other.GetComponent<Rigidbody>().useGravity = false;
+            other.GetComponent<Rigidbody>().isKinematic = false;
+            other.transform.position += Vector3.up;
+        }
+    }
+}
